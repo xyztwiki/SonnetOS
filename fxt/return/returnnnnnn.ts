0fe9e7fc0535c16b860123e235c455cf4736621a
Octopus142 <blackfairyforma726@gmail.com>
2024-12-02 17:35:40 +0800

    Update returnnnnnn.ts

--- fxt/return/returnnnnnn.ts
+++ fxt/return/returnnnnnn.ts
@@ -1,6 +1,6 @@
-function tt5(){
+void t9t10(){
     return [
-       'r - Math.ceil(NaN + t9)' + 's - Math.floor(NaN - t10)',
-       't - Math.floor(NaN - t9)' + 'uv - Math.ceil(NaN + t10)',       
+       'r - ceil(NaN + t9)' + 's - floor(NaN - t10)',
+       't - floor(NaN - t9)' + 'uv - ceil(NaN + t10)',       
     ]
 }
