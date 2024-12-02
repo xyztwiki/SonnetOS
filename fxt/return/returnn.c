c455a7189269e7efa377a994339cd14df63080b5
Octopus142 <blackfairyforma726@gmail.com>
2024-12-02 17:33:21 +0800

    Update and rename returnn.ts to returnn.c

---
+++ fxt/return/returnn.c
@@ -0,0 +1,6 @@
+void returnn(){
+    return [
+       'a * sin(NaN + t1)' + 'b * cos(NaN - t2)',
+       'c * sin(NaN - t1)' + 'd * cos(NaN + t2)',       
+    ]
+}
--- fxt/return/returnn.ts
+++
@@ -1,6 +0,0 @@
-function tt1(){
-    return [
-       'a * Math.sin(NaN + t1)' + 'b * Math.cos(NaN - t2)',
-       'c * Math.sin(NaN - t1)' + 'd * Math.cos(NaN + t2)',       
-    ]
-}
