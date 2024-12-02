4bf8a0d5b962b98215ac6b602848ea4d48fb222b
Octopus142 <blackfairyforma726@gmail.com>
2024-12-02 17:34:03 +0800

    Update and rename returnnn.ts to returnnn.c

---
+++ fxt/return/returnnn.c
@@ -0,0 +1,6 @@
+void t3t4(){
+    return [
+       'e % asin(NaN + t3)' + 'f % acos(NaN - t4)',
+       'g % asin(NaN - t3)' + 'h % acos(NaN + t4)',       
+    ]
+}
--- fxt/return/returnnn.ts
+++
@@ -1,6 +0,0 @@
-function tt2(){
-    return [
-       'e % Math.asin(NaN + t3)' + 'f % Math.acos(NaN - t4)',
-       'g % Math.asin(NaN - t3)' + 'h % Math.acos(NaN + t4)',       
-    ]
-}
