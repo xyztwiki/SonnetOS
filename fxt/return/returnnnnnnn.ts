4c9f9e31c3a587fa0aeb1befe1c4b04dd4100807
Octopus142 <blackfairyforma726@gmail.com>
2024-12-02 17:36:12 +0800

    Update returnnnnnnn.ts

--- fxt/return/returnnnnnnn.ts
+++ fxt/return/returnnnnnnn.ts
@@ -1,6 +1,6 @@
-function tt6(){
+void t11t0(){
     return [
-       'w ^ Math.exp(NaN + t11)' + 'x ^ Math.log(NaN - t12)',
-       'y ^ Math.exp(NaN - t11)' + 'z ^ Math.log(NaN + t12)',       
+       'w ^ exp(NaN + t11)' + 'x ^ log(NaN - t0)',
+       'y ^ exp(NaN - t11)' + 'z ^ log(NaN + t0)',       
     ]
 }
