d75a6efc29c4306a75427b8b205039fd7648cb38
xyztwiki <blackfairyforma726@gmail.com>
2025-02-28 12:52:37 +0800

    Update and rename fx789101112.ts to fw789101112.ts

---
+++ fxt/fw789101112.ts
@@ -0,0 +1,8 @@
+import "XYZ.ts"
+import "bin.c"
+const fw7 = 'x1^7' + 'w1 * x1^3' + 'w2 * y1^2' + 'w3 * z1';
+const fw8 = "x1^7" + "w1' * x1^3" + "w2' * y1^2" + "w3 * z1";
+const fw9 = 'x2^7' + 'w1 * x2^3' + 'w2 * y2^2' + 'w3 * z1';
+const fw10 = "x2^7" + "w1' * x2^3" + "w2' * y2^2" + "w3' * z2"; 
+const fw11 = 'x3^7' + 'w1 * x3^3' + 'w2 * y3^2' + 'w3 * z3';
+const fw12 = "x3^7" + "w1' * x3^3" + "w2' * y3^2" + "w3' * z3";
--- fxt/fx789101112.ts
+++
@@ -1,8 +0,0 @@
-import "XYZ.ts"
-import "bin.c"
-const fx7 = 'x1^7' + 'w1 * x1^3' + 'w2 * y1^2' + 'w3 * z1';
-const fx8 = "x1^7" + "w1' * x1^3" + "w2' * y1^2" + "w3 * z1";
-const fx9 = 'x2^7' + 'w1 * x2^3' + 'w2 * y2^2' + 'w3 * z1';
-const fx10 = "x2^7" + "w1' * x2^3" + "w2' * y2^2" + "w3' * z2"; 
-const fx11 = 'x3^7' + 'w1 * x3^3' + 'w2 * y3^2' + 'w3 * z3';
-const fx12 = "x3^7" + "w1' * x3^3" + "w2' * y3^2" + "w3' * z3";
