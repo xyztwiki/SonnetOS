6b5708771e78386cec70fdaf7e1c58dfb38027e0
xyztwiki <blackfairyforma726@gmail.com>
2025-02-28 12:52:19 +0800

    Update fx123456.ts

--- fxt/fx123456.ts
+++ fxt/fx123456.ts
@@ -1,8 +1,8 @@
 import "XYZ.ts"
 import "w.c"
-const fx1 = 'w1*x1' + 'w2*y1' + 'w3*z1';
-const fx2 = "w1'*x1" + "w2'*y1" + "w3'*z1";
-const fx3 = 'w1*x2' + 'w2*y2' + 'w3*z3';
-const fx4 = "w1'*x2" + "w2'*y2" + "w3'*z2";
-const fx5 = 'w1*x3' + 'w2*y3' + 'w3*z3';
-const fx6 = "w1'*x3" + "w2'*z2" + "w3'*z3";
+const fw1 = 'w1*x1' + 'w2*y1' + 'w3*z1';
+const fw2 = "w1'*x1" + "w2'*y1" + "w3'*z1";
+const fw3 = 'w1*x2' + 'w2*y2' + 'w3*z3';
+const fw4 = "w1'*x2" + "w2'*y2" + "w3'*z2";
+const fw5 = 'w1*x3' + 'w2*y3' + 'w3*z3';
+const fw6 = "w1'*x3" + "w2'*z2" + "w3'*z3";
